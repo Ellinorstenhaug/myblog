700b9cea8944ee46a853505d6bc6861fa14b6079
aelex91 <alexanderhelsinghof@hotmail.com>
2018-08-14 22:28:17 +0200

    Init project

---
+++ MyBlog/Controllers/HomeController.cs
@@ -0,0 +1,26 @@
+﻿using MyBlog.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MyBlog.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Home/
+
+        
+        public ActionResult Index()
+        {
+            var user = new User()
+            {
+                Name = "elky"
+            };
+            return View(user);
+        }
+
+    }
+}
